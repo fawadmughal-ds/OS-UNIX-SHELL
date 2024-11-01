0135beedd093f070930c51c45788916eb190cb38
Fawad Mughal <fawadmughal0333@gmail.com>
2024-11-01 21:26:21 +0500

    Version 5 COmpleted !

--- shellv5.c
+++ shellv5.c
@@ -11,3 +11,26 @@ void execute_builtin(char **args) {
     }
 }
 
+int main() {
+    char input[MAX_CMD_LEN];
+    char *args[MAX_ARGS];
+
+    while (1) {
+        display_prompt();
+        if (fgets(input, MAX_CMD_LEN, stdin) == NULL) {
+            printf("\nExiting shell...\n");
+            break;
+        }
+        add_to_history(input);
+        if (parse_command(input, args) > 0) {
+            if (args[0][0] == '!') {
+                execute_history(args);
+            } else if (strcmp(args[0], "cd") == 0 || strcmp(args[0], "exit") == 0 || strcmp(args[0], "help") == 0) {
+                execute_builtin(args);
+            } else {
+                execute_command_v3(args);
+            }
+        }
+    }
+    return 0;
+}
